910c781e1d933504d4fa57b20b71e92d0d295584
duongnv <nguyenduong985911@gmail.com>
2022-06-24 17:31:06 +0700

    d

--- src/main/java/com/duongnv/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.duongnv.service;
-
-import com.duongnv.model.UserModel;
-
-public interface IUserService {
-	UserModel findByUsernamePassWordStatus(String username, String password, Integer status);
-}
--- src/main/java/com/duongnv/service/impl/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.duongnv.service.impl;
-
-import com.duongnv.service.IStudentService;
-
-public class StudentService implements IStudentService {
-
-}
