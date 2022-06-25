53dec94eb3aac78ad6cc38f402a28a385585e2b0
duongnv <nguyenduong985911@gmail.com>
2022-06-25 15:53:02 +0700

    delete all

--- src/main/java/com/duongnv/dao/IUserDao.java
+++
@@ -1,7 +0,0 @@
-package com.duongnv.dao;
-
-import com.duongnv.model.UserModel;
-
-public interface IUserDao {
-	UserModel findByUsernamePassWordStatus(String username, String password, Integer status);
-}
